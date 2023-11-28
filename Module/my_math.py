99fbe666f3d213a7baf06ebb78cfbb0c3cec1b59
Abdulkadir Gobena Denboba <86346817+Abdulke96@users.noreply.github.com>
2023-11-28 14:16:00 +0000

    module

---
+++ Module/my_math.py
@@ -0,0 +1,10 @@
+def summation(a, b):
+    return a+b
+def substraction (a,b):
+    return a-b
+def multiplication(a, b):
+    return a*b
+def division(a, b):
+    if b == 0:
+        return "division by zero"
+    return a/b
---
+++ Module/simple_calulator.py
@@ -0,0 +1,13 @@
+from my_math import summation as sum
+from my_math import substraction as sub
+from my_math import multiplication as mul
+from my_math import division as div
+
+a = 23 
+b = 7
+print(f"\n the sum of {a}  and {b} is : {sum(a,b)}")
+print(f" the mulof {a}  and {b} is : {mul(a,b)}")
+print(f" the sub of {a}  and {b} is : {sub(a,b)}")
+print(f" the div of {a}  and {b} is : {div(a,b)}\n")
+
+
